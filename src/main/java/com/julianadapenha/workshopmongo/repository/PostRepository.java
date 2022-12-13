4c5eb929eb5cb85719565a4dc153a51a74f39cb3
JulianaDaPenha <julianadapenha@gmail.com>
2022-12-13 16:40:33 -0300

    Consulta simples com query methods

--- src/main/java/com/julianadapenha/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/julianadapenha/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.julianadapenha.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.julianadapenha.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
